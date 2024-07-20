ee5e1b0bed076d9ab199e783d7e0055c3a826250
sandhyat <sandhya.tripathi8@gmail.com>
2024-07-20 15:22:07 -0500

    --the file now takes the task and model argument--

--- End_to_end_supervised/ts_optuna_hp_tuning_combined.sh
+++ End_to_end_supervised/ts_optuna_hp_tuning_combined.sh
@@ -1,11 +1,15 @@
 # this file is going to take the input of random numbers and the ablation details
 # this file is expected to run 30 trials (5 random initialization times 6 ablation cases)f or one outcome and one model
 
-# to start the docker container inside which this file will run
-#docker run -it --gpus all --privileged -v '/home/trips/PeriOperative_RiskPrediction/:/codes/' -v '/mnt/ris/ActFastExports/v1.3.2/:/input/' -v '/home/trips/PeriOperative_RiskPrediction/HP_output/:/output/' docker121720/pytorch-for-ts:0.95 /bin/bash
 
+# first positional argument will be the task second positional argument will be the model with options {'lstm', 'transformer'}
 
 #!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "No task provided."
+    exit 1
+fi
 
 pip install optuna
 #pip uninstall pandas
@@ -16,10 +20,12 @@ numbers=($(shuf -i 100-500 -n 5))
 for number in "${numbers[@]}"
 do
   echo $number
-  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --task='icu' --randomSeed=$number
-  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --flow --task='icu' --randomSeed=$number
-  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --task='icu' --randomSeed=$number
-  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --preops --task='icu' --randomSeed=$number
-  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --preops --homemeds --task='icu' --randomSeed=$number
-  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --preops --homemeds --pmhProblist --task='icu' --randomSeed=$number
+  echo $1
+  echo $2
+  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --task=$1 --modelType=$2 --randomSeed=$number
+  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --flow --task=$1 --modelType=$2 --randomSeed=$number
+  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --task=$1 --modelType=$2 --randomSeed=$number
+  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --preops --task=$1 --modelType=$2 --randomSeed=$number
+  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --preops --homemeds --task=$1 --modelType=$2 --randomSeed=$number
+  python /codes/End_to_end_supervised/Training_with_ts_Optuna_hp_tuning.py --meds --flow --preops --homemeds --pmhProblist --task=$1 --modelType=$2 --randomSeed=$number
 done
